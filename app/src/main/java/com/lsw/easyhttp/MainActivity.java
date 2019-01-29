67adf8eca410f61f739649e1ec984797acdbbbb6
sweeneyliu <sweeneyliu@tencent.com>
2019-01-29 17:54:42 +0800

    增加Okhttp的简易封装

--- app/src/main/java/com/lsw/easyhttp/MainActivity.java
+++ app/src/main/java/com/lsw/easyhttp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.lsw.easyhttp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.lsw.easyhttp.http.HttpProcessor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private static final String HENCODER_URL = "https://api.hencoder.com/author";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
     }
 }
--- app/src/main/java/com/lsw/easyhttp/processor/OkHttpProcessor.java
+++ app/src/main/java/com/lsw/easyhttp/processor/OkHttpProcessor.java
@@ -4,7 +4,12 @@ import com.lsw.easyhttp.interfaces.ICallBack;
 import com.lsw.easyhttp.interfaces.IhttpProcessor;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -23,6 +28,59 @@ public class OkHttpProcessor implements IhttpProcessor{
 
     public OkHttpProcessor() {
         mOkHttpClient = new OkHttpClient();
+        okhttp3.OkHttpClient.Builder ClientBuilder=new okhttp3.OkHttpClient.Builder();
+        ClientBuilder.readTimeout(20, TimeUnit.SECONDS);//读取超时
+        ClientBuilder.connectTimeout(6, TimeUnit.SECONDS);//连接超时
+        ClientBuilder.writeTimeout(60, TimeUnit.SECONDS);//写入超时
+        //todo 支持HTTPS请求，跳过证书验证
+        mOkHttpClient=ClientBuilder.build();
+    }
+
+    /**
+     * get请求，同步方式，获取网络数据，是在主线程中执行的，需要新起线程，将其放到子线程中执行
+     * @param url
+     * @return
+     */
+    public  Response getDataSynFromNet(String url) {
+        //1 构造Request
+        Request.Builder builder = new Request.Builder();
+        Request request=builder.get().url(url).build();
+        //2 将Request封装为Call
+        Call call = mOkHttpClient.newCall(request);
+        //3 执行Call，得到response
+        Response response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    /**
+     * get请求，异步方式，获取网络数据，是在子线程中执行的，需要切换到主线程才能更新UI
+     * @param url
+     * @param callback
+     * @return
+     */
+    public  void getDataAsynFromNet(String url, final ICallBack callback) {
+        //1 构造Request
+        Request.Builder builder = new Request.Builder();
+        Request request=builder.get().url(url).build();
+        //2 将Request封装为Call
+        Call call = mOkHttpClient.newCall(request);
+        //3 执行Call
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+//                myNetCall.failed(call,e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+//                myNetCall.success(call,response);
+            }
+        });
     }
 
     //get异步调用
@@ -48,7 +106,58 @@ public class OkHttpProcessor implements IhttpProcessor{
         });
     }
 
-    //todo get同步调用
+    /**
+     * post请求，同步方式，提交数据，是在主线程中执行的，需要新起线程，将其放到子线程中执行
+     * @param url
+     * @param bodyParams
+     * @return
+     */
+    public Response postDataSynToNet(String url, Map<String, String> bodyParams) {
+        //1构造RequestBody
+        RequestBody body = setRequestBody(bodyParams);
+        //2 构造Request
+        Request.Builder requestBuilder = new Request.Builder();
+        Request request = requestBuilder.post(body).url(url).build();
+        //3 将Request封装为Call
+        Call call = mOkHttpClient.newCall(request);
+        //4 执行Call，得到response
+        Response response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    /**
+     * post请求，异步方式，提交数据，是在子线程中执行的，需要切换到主线程才能更新UI
+     * @param url
+     * @param bodyParams
+     * @param callback
+     */
+    public  void postDataAsynToNet(String url, Map<String,String> bodyParams, final ICallBack callback) {
+        //1构造RequestBody
+        RequestBody body=setRequestBody(bodyParams);
+        //2 构造Request
+        Request.Builder requestBuilder = new Request.Builder();
+        Request request = requestBuilder.post(body).url(url).build();
+        //3 将Request封装为Call
+        Call call = mOkHttpClient.newCall(request);
+        //4 执行Call
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+//                myNetCall.failed(call,e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+//                myNetCall.success(call,response);
+
+            }
+        });
+    }
 
     @Override
     public void post(String url, Map<String, Object> params, final ICallBack callback) {
@@ -88,5 +197,26 @@ public class OkHttpProcessor implements IhttpProcessor{
             body.add(entry.getKey(),entry.getValue().toString());
         }
         return body.build();
+    }
+
+    /**
+     * post的请求参数，构造RequestBody
+     * @param BodyParams
+     * @return
+     */
+    private RequestBody setRequestBody(Map<String, String> BodyParams){
+        RequestBody body=null;
+        okhttp3.FormBody.Builder formEncodingBuilder=new okhttp3.FormBody.Builder();
+        if(BodyParams != null){
+            Iterator<String> iterator = BodyParams.keySet().iterator();
+            String key = "";
+            while (iterator.hasNext()) {
+                key = iterator.next().toString();
+                formEncodingBuilder.add(key, BodyParams.get(key));
+            }
+        }
+        body=formEncodingBuilder.build();
+        return body;
+
     }
 }
